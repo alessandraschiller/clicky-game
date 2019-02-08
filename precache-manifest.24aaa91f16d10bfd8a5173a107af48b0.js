eb97de2a11e804e563122566ddd866b41b22cc61
Alessandra Schiller <a.schiller@umiami.edu>
2019-02-08 04:23:13 -0500

    Updates

---
+++ precache-manifest.24aaa91f16d10bfd8a5173a107af48b0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "d14a4b87e8f74f2a9771",
+    "url": "/clicky-game/static/css/main.8031e211.chunk.css"
+  },
+  {
+    "revision": "d14a4b87e8f74f2a9771",
+    "url": "/clicky-game/static/js/main.d14a4b87.chunk.js"
+  },
+  {
+    "revision": "0df4cdf2d8ed9cd52016",
+    "url": "/clicky-game/static/js/1.0df4cdf2.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8fa0a1381eed375eea7ddc1f699249d9",
+    "url": "/clicky-game/static/media/background.8fa0a138.jpeg"
+  },
+  {
+    "revision": "f794e987116fef4e97aea83325d753b2",
+    "url": "/clicky-game/static/media/boa.f794e987.jpg"
+  },
+  {
+    "revision": "21eaff128d91e6d90c142b1d81186e14",
+    "url": "/clicky-game/static/media/bubbles.21eaff12.jpg"
+  },
+  {
+    "revision": "c8d2b8e901e274e347cc69314425caad",
+    "url": "/clicky-game/static/media/dutchman.c8d2b8e9.jpg"
+  },
+  {
+    "revision": "97d7c6af9ca93222311a582086bf4756",
+    "url": "/clicky-game/static/media/easter.97d7c6af.jpg"
+  },
+  {
+    "revision": "5e6dd2f7be488a6457cc91629a02af3f",
+    "url": "/clicky-game/static/media/jellyfish2.5e6dd2f7.jpg"
+  },
+  {
+    "revision": "6d0ad36919e1895f93e78a6105b9899c",
+    "url": "/clicky-game/static/media/jellyfishing.6d0ad369.jpg"
+  },
+  {
+    "revision": "0f55252952f3cefd0e38da9107aed02b",
+    "url": "/clicky-game/static/media/nakeyy.0f552529.jpg"
+  },
+  {
+    "revision": "b8edc8091fd7f39fa67ea032bd21b6a3",
+    "url": "/clicky-game/static/media/spongebobhula.b8edc809.jpg"
+  },
+  {
+    "revision": "f26dfaf21c6c72690b32439108ea8498",
+    "url": "/clicky-game/static/media/spongebobpat.f26dfaf2.jpg"
+  },
+  {
+    "revision": "3334aa13fa81e2f989f0ca94b6f87d50",
+    "url": "/clicky-game/static/media/spongeburgers.3334aa13.jpg"
+  },
+  {
+    "revision": "05a77af46348a78c28d8a8b530aef6e8",
+    "url": "/clicky-game/static/media/squid.05a77af4.jpg"
+  },
+  {
+    "revision": "def54361e03661bd7257dc7e50d6e133",
+    "url": "/clicky-game/static/media/wideeyed.def54361.jpg"
+  },
+  {
+    "revision": "90003fa61970c33d98aafb3923a7ff56",
+    "url": "/clicky-game/index.html"
+  }
+];
